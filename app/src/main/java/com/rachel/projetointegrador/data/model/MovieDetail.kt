a757d8294c31e86fa7970954307c8fc034a4ca09
Rachel Barros <rachelnmlb@hotmail.com>
2021-08-12 22:23:56 -0300

    Incluindo o ano de lançamento nos detalhes do filme

--- app/src/main/java/com/rachel/projetointegrador/data/model/MovieDetail.kt
+++ app/src/main/java/com/rachel/projetointegrador/data/model/MovieDetail.kt
@@ -1,9 +1,6 @@
 package com.rachel.projetointegrador.data.model
 
-import com.google.gson.annotations.JsonAdapter
 import com.google.gson.annotations.SerializedName
-import com.google.gson.internal.bind.DateTypeAdapter
-import java.util.*
 
 data class MovieDetail(
     @SerializedName("id") val id : Int,
@@ -11,9 +8,14 @@ data class MovieDetail(
     @SerializedName("poster_path") val posterPath: String? = null,
     @SerializedName("title") val title: String? = null,
     @SerializedName("vote_average") val rating: Float,
-    @SerializedName("release_date") val releaseDate: Date? = null,
+    @SerializedName("release_date") val releaseDate: String? = null,
     @SerializedName("runtime") val duration: Int? = null,
     @SerializedName("genres") val genres: List<Genre>,
     @SerializedName("overview") val overview: String? = null,
     var isFavorite: Boolean = false
-    )
+    ) {
+
+    fun getReleaseYear(): String {
+        return releaseDate?.substring(0, 4) ?: ""
+    }
+}
--- app/src/main/java/com/rachel/projetointegrador/presentation/DetailActivity.kt
+++ app/src/main/java/com/rachel/projetointegrador/presentation/DetailActivity.kt
@@ -69,6 +69,7 @@ class DetailActivity: AppCompatActivity() {
                 binding.txtMovieTitle.text = movieDetail.title
                 binding.txtSinopsys.text = movieDetail.overview
                 binding.favoriteDetail.isChecked = movieDetail.isFavorite
+                binding.txtMovieYear.text = movieDetail.getReleaseYear()
 
                 val rating = movieDetail.rating * 10.0
                 binding.txtNumberPercent.text = "${"%.0f".format(rating)}%"
